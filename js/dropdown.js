2234d7584f1abe3b929c114abdbc9286fa233e9a
zd3g11 <zd3g11@gmail.com>
2019-07-23 22:30:03 +0900

    add 7-23

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,7 @@
-$(function(){
-    $('.main-list li').hover(function(){
-        $("ul:not(:animated)", this).slideDown(70);
-    }, function(){
-        $("ul.dropdwn_menu",this).slideUp(70);
-    });
-});
+ $(function(){
+     $('.main-list li').hover(function(){
+         $("ul:not(:animated)", this).slideDown(70);
+     }, function(){
+         $("ul.dropdwn_menu",this).slideUp(70);
+     });
+ });
